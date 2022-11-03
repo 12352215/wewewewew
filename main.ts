0147ee3d4a6787108952a2f9d4ce9ec5af9601a6
12352215 <114720970+12352215@users.noreply.github.com>
2022-11-04 07:48:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,39 @@
 input.onButtonPressed(Button.A, function () {
-    powerbrick.MotorRun(powerbrick.Motors.M1, -103)
-    basic.pause(500)
-    powerbrick.MotorStop(powerbrick.Motors.M1)
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
 })
 input.onButtonPressed(Button.B, function () {
-    powerbrick.MotorRun(powerbrick.Motors.M1, 103)
-    basic.pause(500)
-    powerbrick.MotorStop(powerbrick.Motors.M1)
+	
 })
 let s = 0
 powerbrick.GC_MODE(powerbrick.GCMode.Gesture)
-powerbrick.Servo(powerbrick.Servos.S1, 140)
+basic.forever(function () {
+    powerbrick.GC_MODE(powerbrick.GCMode.Gesture)
+    basic.showNumber(powerbrick.GC_Gesture())
+})
+basic.forever(function () {
+    if (powerbrick.GC_Gesture() == 3) {
+        powerbrick.MotorRun(powerbrick.Motors.M1, 255)
+        basic.pause(200)
+        powerbrick.MotorStop(powerbrick.Motors.M1)
+    }
+    if (powerbrick.GC_Gesture() == 1) {
+        powerbrick.MotorRun(powerbrick.Motors.M1, -160)
+        basic.pause(200)
+        powerbrick.MotorStop(powerbrick.Motors.M1)
+    }
+    if (powerbrick.GC_Gesture() == 4) {
+        powerbrick.MotorRun(powerbrick.Motors.M2, -214)
+        basic.pause(200)
+        powerbrick.MotorStop(powerbrick.Motors.M2)
+    }
+})
 basic.forever(function () {
     s = powerbrick.GC_Gesture()
-    if (s == 1) {
-        basic.showArrow(ArrowNames.North)
-        powerbrick.GC_MODE(powerbrick.GCMode.Gesture)
-    }
     if (s == 2) {
-        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.West)
         powerbrick.GC_MODE(powerbrick.GCMode.Gesture)
     }
     if (s == 3) {
@@ -26,21 +41,12 @@ basic.forever(function () {
         powerbrick.GC_MODE(powerbrick.GCMode.Gesture)
     }
     if (s == 4) {
-        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.North)
         powerbrick.GC_MODE(powerbrick.GCMode.Gesture)
     }
-})
-basic.forever(function () {
-    if (powerbrick.GC_Gesture() == 2) {
-    	
-    }
-    if (powerbrick.GC_Gesture() == 4) {
-    	
-    }
-})
-basic.forever(function () {
     s = powerbrick.GC_Gesture()
-    if (s == 3) {
-        powerbrick.Servo(powerbrick.Servos.S1, 140)
+    if (s == 1) {
+        basic.showArrow(ArrowNames.East)
+        powerbrick.GC_MODE(powerbrick.GCMode.Gesture)
     }
 })
